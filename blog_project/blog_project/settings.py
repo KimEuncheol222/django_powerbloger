3aa4422fdfc9eb9b60449828f1a52aaaf8fb04f9
Yumi Heo <hymsimple@gmail.com>
2023-09-11 14:28:42 +0900

    [Feat] DB 설정 추가

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_project',
+        'USER': 'blog_project_user',
+        'PASSWORD': '',
+        'HOST': '', # DB관리자IP
+        'PORT': '5432',
     }
 }
 
