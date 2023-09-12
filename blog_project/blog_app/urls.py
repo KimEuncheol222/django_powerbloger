c657778e03b83b974f837ee286ea60115b65469b
Yumi Heo <hymsimple@gmail.com>
2023-09-12 12:32:36 +0900

    [Add] basic templates, url connection

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.board_client, name='board_client'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('board_admin', views.board_admin, name='board_admin'),
+    path('post', views.post, name='post'),
+    path('write', views.write, name='write'),
 ]
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def login(request):
+    return render(request, 'registration/login.html')
+
+def signup(request):
+    return render(request, 'registration/signup.html')
+
+def board_client(request):
+    return render(request, 'blog_app/board_client.html')
+
+def board_admin(request):
+    return render(request, 'blog_app/board_admin.html')
+
+def post(request):
+    return render(request, 'blog_app/post.html')
+
+def write(request):
+    return render(request, 'blog_app/write.html')
