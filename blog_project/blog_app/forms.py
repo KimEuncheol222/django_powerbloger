98be049b518f14a69b122df366f79e717347349c
Yumi Heo <hymsimple@gmail.com>
2023-09-13 15:03:26 +0900

    [Feat] topic 라디오버튼

--- blog_project/blog_app/forms.py
+++ blog_project/blog_app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import BlogPost
+from .models import BlogPost, Topic
 
 class BlogPostForm(forms.ModelForm):
+    topic = forms.ModelChoiceField(
+        queryset=Topic.objects.all(),
+        widget=forms.RadioSelect,
+        to_field_name='name'
+    )
     class Meta:
         model = BlogPost
         fields = ['title', 'content', 'topic', 'image']
--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -6,6 +6,8 @@ from django.conf import settings
 
 class Topic(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.name
 
 
 class BlogPost(models.Model):
